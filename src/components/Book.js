06d147ba53be4a871f43e5c2699bc3afc445b698
kifle23 <68891198+kifle23@users.noreply.github.com>
2023-06-15 17:40:40 +0300

    Refactor code to use Bookstore API

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,31 +1,85 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/booksSlice';
-import './Book.css';
-import BookRecord from './BookRecord';
+import { addBook, removeBook } from '../redux/books/booksSlice';
+import './BookRecord.css';
 
 function Book() {
-  const books = useSelector((state) => state.books || []);
+  const { books, isLoading, errorMsg } = useSelector(
+    (state) => state.books || [],
+  );
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
-  const [type] = useState('action');
+  const [category] = useState('Action');
+
+  if (isLoading) {
+    return <div style={{ marginTop: '50px' }}>Loading...</div>;
+  }
+
+  if (errorMsg) {
+    return <div style={{ marginTop: '50px' }}>Failed to fetch</div>;
+  }
+
+  const handleAddBook = async () => {
+    if (author !== '' && title !== '') {
+      dispatch(
+        addBook({
+          item_id: uuidv4(),
+          title,
+          author,
+          category,
+        }),
+      )
+        .unwrap()
+        .then(() => {
+          setTitle('');
+          setAuthor('');
+        });
+    }
+  };
+
   return (
     <>
       <section className="section">
         <div className="container">
-          <div>
-            {books.map((book) => (
-              <BookRecord
-                type={book.type}
-                title={book.title}
-                author={book.author}
-                key={book.id}
-                id={book.id}
-              />
-            ))}
-          </div>
+          {Object.entries(books).map(([id, book]) => book.map((bookList) => (
+            <div className="content" key={id}>
+              <div className="aside">
+                <div className="aside_left">
+                  <div>{bookList.category}</div>
+                  <div>{bookList.title}</div>
+                  <div>{bookList.author}</div>
+                  <button type="button">Comments</button>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      dispatch(removeBook(id));
+                    }}
+                  >
+                    Remove
+                  </button>
+                  <button type="button">Edit</button>
+                </div>
+                <div className="aside_center">
+                  <div className="percent">
+                    <div className="loader">
+                      <div className="box">Progress Graph</div>
+                    </div>
+                    <div className="">
+                      <span id="percent_value">64%</span>
+                      <span id="percent_status">Completed</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="aside_right">
+                  <p>CURRENT CHAPTER</p>
+                  <span>Chapter 17</span>
+                  <button type="button">UPDATE PROGRESS</button>
+                </div>
+              </div>
+            </div>
+          )))}
         </div>
       </section>
       <hr />
@@ -39,6 +93,7 @@ function Book() {
                 className="input-text"
                 placeholder="Add title"
                 name="title"
+                value={title}
                 onChange={(e) => {
                   setTitle(e.target.value);
                 }}
@@ -48,26 +103,12 @@ function Book() {
                 className="input-text"
                 placeholder="Add author"
                 name="author"
+                value={author}
                 onChange={(e) => {
                   setAuthor(e.target.value);
                 }}
               />
-              <button
-                type="button"
-                onClick={() => {
-                  dispatch(
-                    addBook({
-                      id: uuidv4(),
-                      type,
-                      title,
-                      author,
-                    }),
-                  );
-                  setTitle('');
-                  setAuthor('');
-                  document.getElementById('addBook').reset();
-                }}
-              >
+              <button type="button" onClick={handleAddBook}>
                 Add
               </button>
             </div>
--- src/components/BookRecord.js
+++ src/components/BookRecord.js
@@ -5,49 +5,54 @@ import './BookRecord.css';
 import { removeBook } from '../redux/books/booksSlice';
 
 function BookRecord({
-  id, type, title, author,
+  id = '', category = '', title = '', author = '',
 }) {
   const dispatch = useDispatch();
+
   return (
-    <>
-      <div className="content">
-        <div className="aside">
-          <div className="aside_left">
-            <div>{type}</div>
-            <div>{title}</div>
-            <div>{author}</div>
-            <button type="button">Comments</button>
-            <button type="button" onClick={() => dispatch(removeBook({ id }))}>
-              Remove
-            </button>
-            <button type="button">Edit</button>
-          </div>
-          <div className="aside_center">
-            <div className="percent">
-              <div className="loader">
-                <div className="box">Progress Graph</div>
-              </div>
-              <div className="">
-                <span id="percent_value">64%</span>
-                <span id="percent_status">Completed</span>
-              </div>
+    <div className="content">
+      <div className="aside">
+        <div className="aside_left">
+          <div>{category}</div>
+          <div>{title}</div>
+          <div>{author}</div>
+          <button type="button">Comments</button>
+          <button
+            type="button"
+            onClick={() => {
+              dispatch(removeBook(id));
+            }}
+          >
+            Remove
+          </button>
+          <button type="button">Edit</button>
+        </div>
+        <div className="aside_center">
+          <div className="percent">
+            <div className="loader">
+              <div className="box">Progress Graph</div>
+            </div>
+            <div className="">
+              <span id="percent_value">64%</span>
+              <span id="percent_status">Completed</span>
             </div>
           </div>
-          <div className="aside_right">
-            <p>CURRENT CHAPTER</p>
-            <span>Chapter 17</span>
-            <button type="button">UPDATE PROGRESS</button>
-          </div>
+        </div>
+        <div className="aside_right">
+          <p>CURRENT CHAPTER</p>
+          <span>Chapter 17</span>
+          <button type="button">UPDATE PROGRESS</button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
 BookRecord.propTypes = {
   id: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
+
 export default BookRecord;
